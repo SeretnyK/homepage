623dc5dfd81394f2d2f0f24c779b40332c9ef4a8
Kacper Seretny <kacper.seretny@gmail.com>
2019-11-09 19:27:46 +0000

    initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+console.log('Siema, wiedze że patrzysz w konsole! 😊');
+const name='Kacper Seretny';
+const age= '21';
+let kacper = null;
+console.log(name);
+console.log(age);
+
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
+
+console.log('Witaj 👌');
