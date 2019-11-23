1ba6b05e0b0f01ff5357f8d0079d5d7e05ca3f2c
Kacper Seretny <kacper.seretny@gmail.com>
2019-11-23 18:53:57 +0000

    JS functions

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 console.log('Siema, wiedze że patrzysz w konsole! 😊');
 const name='Kacper Seretny';
-const age= '21';
+const age= '75';
 let kacper = null;
 console.log(name);
 console.log(age);
@@ -21,7 +21,58 @@ console.log(paragraphs[0]);
 
 
 const paragraph__js = document.querySelector('.paragraph__js')
-
-console.log(paragraph__js.innerHTML)
-
 paragraph__js.innerHTML = `Nazywam się <strong>${name}</strong> i mam<strong> ${age}</strong> lat`
+
+if (age < 20) {
+    console.log('masz mniej niż 20 lat');
+} else if ((age >=20 && (age <=30))) {
+    console.log('masz wiecej niż 20 lat ale mniej niż 30')
+} else {
+console.log('jesteś 31+');
+}
+
+
+switch (age) {
+    case 21:
+        console.log('masz równo 21 lat')
+    break;
+    case 30:
+        console.log('masz równo 30 lat')
+    break;
+    default:
+        console.log(`masz równo ${age} lat`);
+    break;
+}
+
+
+
+let oldIndicator;
+
+if (age > 70) {
+  oldIndicator = 'yes';
+} else {
+  oldIndicator = 'no'
+}
+
+
+
+const amIOld = (age > 70) ? 'yes' : 'no'
+console.log(amIOld);
+
+
+function calculate(x) {
+  x = x + 3;
+  console.log(`tradycyjnie ${x}`);
+  return x*7
+}
+
+calculate(1);
+
+console.log(calculate(2));
+const myCalculation = calculate(4);
+console.log(myCalculation);
+
+
+const calculateFat = x => (x+3)*7;
+
+console.log(calculateFat(2))
